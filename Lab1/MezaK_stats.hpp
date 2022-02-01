dce7667975e6bee1c1f42b97981c0b417de8adb2
Kat <kathya.meza@marquette.edu>
2022-02-01 16:51:07 -0600

    Added ERP files + code to stat.hpp and stat.cpp

--- Lab1/MezaK_stats.hpp
+++ Lab1/MezaK_stats.hpp
@@ -5,3 +5,71 @@ Date: 2/01/22
 Description: simple statistical routines in C++ that will be compiled and 
 stored in your own library
 */
+
+#ifndef MezaK_HeadrGaurd
+#define MezaK_HeadrGaurd
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+namespace KXM{
+
+    class Stats{
+
+            public:
+                    //setters
+                    Stats();
+                    
+                    void minimum(vector<float>);
+                    void maximum(vector<float>);
+                    void mean(vector<float>);
+                    void stdev(vector<float>);
+                    vector<float> histogram(vector<float>);
+                
+            private:
+                    float min;
+                    float max;
+                    float mean;
+    };
+
+}
+
+
+ void KXM::Stats::minimum(std::vector<float> data){
+
+     int size = data.size();
+     int i = 0;
+     float min = data[0];
+
+     while(i < size){
+
+         if(min < data[i]){
+
+            this->min = data[i];
+         }
+         i++;
+     }
+     //return min;
+}
+
+void KXM::Stats::maximum(std::vector<float> data){
+
+     int size = data.size();
+     int i = 0;
+     float max = data[0];
+
+     while(i < size){
+
+         if(max > data[i]){
+
+            this->max = data[i];
+         }
+         i++;
+     }
+     //return max;
+}
+
+#endif
