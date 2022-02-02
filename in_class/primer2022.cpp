a7f621030c2e26c136e531fba64e97471ae765f5
Kat <kathya.meza@marquette.edu>
2022-02-02 10:41:23 -0600

    In Class additions to Primers

--- in_class/primer2022.cpp
+++ in_class/primer2022.cpp
@@ -1,6 +1,12 @@
 
 #include "primer2022.hpp"
 
+mySpace::yourSpace::myClass::myClass(std::string name, int init_num_students){
+
+    this->className = name;
+    this->num_students = init_num_students;
+}
+
 int mySpace::yourSpace::myClass::get_num_students(){
 
     return this->num_students;
--- in_class/primer2022.hpp
+++ in_class/primer2022.hpp
@@ -7,6 +7,7 @@ Description: This code will act as a primer on class design and creation in C++
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 //mySpace::yourSpace::myClass
 
@@ -17,15 +18,20 @@ namespace mySpace{
       public:
          myClass(std::string name, int init_num_students);
             myClass(std::string name);
+            myClass();
+            ~myClass();
 
          int get_num_students();
          std::string get_name();
          void set_num_students(int num);
+         void set_name(std::string newName);
+         std::vector<std::string>* get_classlist(); 
 
       private:
 
          int num_students;
          std::string className;
+         std::vector<std::string> classlist;
 
 };
 }
