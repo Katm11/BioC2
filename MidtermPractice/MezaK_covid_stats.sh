c9a8dacf3d16937401674fb6aeedaf127a891ee6
Kat <kathya.meza@marquette.edu>
2022-03-07 22:09:42 -0600

    Removed unecessary code!

--- MidtermPractice/MezaK_covid_stats.sh
+++ MidtermPractice/MezaK_covid_stats.sh
@@ -40,37 +40,10 @@ echo "${location[@]}" > countries.txt
 echo "${new_cases_per_million[@]}" > newCasesPmil.txt
 echo "${date[@]}" > dates.txt
 
-#headers=={ head -n 1}
-# OLDIFS=$IFS
-# IFS=',/'
-# while IFS='/,' read -ra dates
-# #[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-# do
-#         # display $line or do something with $line
-#      month+=("${dates[0]}")
-#       day+=("${dates[1]}")
-#       year+=("${dates[2]}")
-
-# done < dates.txt
-# IFS=$OLDIFS
-
-# echo "${year[@]}" > year.txt
-# echo "${month[@]}" > month.txt
-# echo "${day[@]}" > day.txt
-#echo $date[@] "+%m"
-#echo dates[@] | sed -r 's/(.{1}).*/\1/'
-#echo dates[@] | sed -r 's//.*//'
-#index=0
 sum=0
 avg=0
 #"${myarr[0]:7:3}" i = 0 ; i <= 1000 ; i++
 
-# read -p "Please enter the reference country name: " refCountry 
-# case $refCountry in  
-#   Australia) start=$(echo "0") ;;
-#   US) start=$(echo "772") ;;
-#   *) echo "Country not found" ;; 
-# esac
 
 case "$1" in
   "Australia")
@@ -92,32 +65,3 @@ done
 avg=$(echo "$sum / $index" | bc -l )
 month=${date[index-2]:0:1}
 echo The average for $1 during ${MONTHS[month]} ${date[index-2]:5:7} is $avg
-
-
-
-#echo ${MONTHS[month]}
-#check for the user input
-# totalCases=0
-# for i in "${date[@]}"; do :
-#     let index++
-#     totalCases=$("$totalCases + $new_cases_per_million[$index]" | bc -l)
-# done
-# avgtotalCases=$(echo "$totalCases / $index" | bc -l)
-
-# echo $avgtotalCases
-# echo "Please enter the header name for the data you'd like to examine"
-# read dataSet
-
-# #error check
-# while [ ! $dataSet == "iso_code" ] && [ ! $dataSet == "continent" ] && [ ! $dataSet == "location" ] && [ ! $dataSet == "date" ] && [ ! $dataSet == "total_cases" ] && [ ! $dataSet == "new_cases" ] && [ ! $dataSet == " total_deaths" ] && [ ! $dataSet == "new_deaths" ] && [ ! $dataSet == "total_cases_per_million" ] && [ ! $dataSet == "new_cases_per_million" ];
-# do
-#     echo "ERROR: Data set not found"               
-#     echo "Please enter a different data set header to try again"
-#     read dataSet
-# done  
-
-# echo "Please enter the reference country name"
-# read refCountry
-
-# echo "Please enter the header name for the name of a comparison country"
-# read compCountry
