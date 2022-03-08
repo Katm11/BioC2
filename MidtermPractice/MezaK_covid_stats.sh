21924b8e73a6e092875018ddef1e53eb0746b72a
Kat <kathya.meza@marquette.edu>
2022-03-08 00:28:25 -0600

    pre-removing blank lines attempt

--- MidtermPractice/MezaK_covid_stats.sh
+++ MidtermPractice/MezaK_covid_stats.sh
@@ -14,14 +14,20 @@ then
     exit 1
 fi
 
-# if [ ! $2 ]
-# then
-#     echo "No second input"
-#     exit 1
-# fi
+if [ ! $2 ]
+then
+    echo "No second input"
+    exit 1
+fi
+
+if [ ! $3 ]
+then
+    echo "No third input"
+    exit 1
+fi
 
 INPUT=owid-covid-data.csv
-
+#sed -i '/^$/d' $INPUT
 OLDIFS=$IFS
 IFS=','
 
@@ -44,24 +50,66 @@ sum=0
 avg=0
 #"${myarr[0]:7:3}" i = 0 ; i <= 1000 ; i++
 
-
-case "$1" in
+case "$2" in
   "Australia")
-    start=$(echo "0") ;;
+    start=$(echo "0") 
+    country="Australia" 
+    ;;
   "US")
-    start=$(echo "772") ;;
+    start=$(echo "772")
+    country="US" 
+    ;;
   *)
-    echo "Country Data Not found"
+    echo "Country Data 1 Not found"
     exit 1
     ;;
 esac
+
+case "$3" in
+  "Australia")
+    start=$(echo "0")
+    country="Australia" 
+    ;;
+  "US")
+    start=$(echo "772")
+    country="US" 
+    ;;
+  *)
+    echo "Country Data 2 Not found"
+    exit 1
+    ;;
+esac
+
+dataSet=$1
+#echo $dataSet
+#$dataSet
+
+
+average_function () {
+    OPTIND=1
 MONTHS=(blank Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
-for ((index = $start ; "${date[index]:0:1}" == "${date[index+1]:0:1}" ; index++))
+
+  for ((i = $start ; "${date[index]:0:1}" == "${date[index+1]:0:1}" ; i++))
 do
-    sum=$(echo "$sum + ${new_cases_per_million[$index]}" | bc -l )
+    sum=$(echo "$sum + ${new_cases_per_million[$i]}" | bc -l )
     echo $sum
-    #index=$((index + 1));
-done 
+    index=$((index + 1));
+done
+
+if [ "${date[index]:0:1}" != "${date[index+1]:0:1}" ]
+then
+    #sum=0
+    index=$((index + 2));
+fi
+
 avg=$(echo "$sum / $index" | bc -l )
-month=${date[index-2]:0:1}
-echo The average for $1 during ${MONTHS[month]} ${date[index-2]:5:7} is $avg
+sum=0
+month=${date[index-1]:0:1}
+#echo $month
+echo The average $1 for $country during ${MONTHS[month]} ${date[index-2]:5:7} is $avg
+
+}
+
+average_function "$1" "$2"
+
+average_function "$1" "$3"
