ee6ead566e3b58a7c25afcb623c511b5d1729f6a
Kat Meza <kathya.meza@marquette.edu>
2022-04-13 21:45:59 -0500

    Initial Commit

---
+++ Mini_Projects/Mini_Project1/MezaK_MiniProject.cpp
@@ -0,0 +1,9 @@
+/*
+Name: Kat Meza
+Assignment: Mini Project 1
+Date: 4/13/22
+Description: To calculate statistics of the airspeed velocity of swallows
+*/ 
+
+#include "/home/mezak/BioC2/Lab1/MezaK_stats.cpp"
+
